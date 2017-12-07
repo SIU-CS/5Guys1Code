c912cde88b9d81142b1c332c2ccdaf1c18a2a7f4
Mohammed <mohd.jkadir@gmail.com>
2017-12-07 14:04:49 -0600

    update on delete button

--- app/theme/toDo.php
+++ app/theme/toDo.php
@@ -1,8 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+    <title>Calendar Organizer</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <link href="assets/css/main.css" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/modernizr.custom.js"></script>
+
+    <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
+  </head>
+<html>
  <?php
 
 $mysql_hostname = "dbserv.cs.siu.edu";
-$mysql_user = ""; // enter your username
-$mysql_password = " "; //enter your password here 
+$mysql_user = " "; // enter your username
+$mysql_password = " "; //enter your password here
 $mysql_database = " "; //enter your db name same as password
 
 // Create connection
@@ -16,9 +40,8 @@ $sql = "SELECT * FROM items";
 $result = mysqli_query($conn, $sql);
 
 mysqli_close($conn);
-?> 
+?>
 
-<html>
 <head>
     <title>To-do</title>
 </head>
@@ -31,24 +54,21 @@ mysqli_close($conn);
             <td>Name</td>
 	    <td>Date</td>
         </tr>
-   <?php
+    </table>
+ <?php
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-       echo " - Name: " . $row["name"]." - Date: " . $row["datee"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-	//echo var_dump ($result);
-
-        while($result = mysqli_fetch_array($result)) {
-            echo "<tr>";
+    while($res = mysqli_fetch_assoc($result)) {
+            echo "" . $row["name"]." ". $row["datee"]."<br>";
+    	    //echo "<tr>";
             echo "<td>".$res['name']."</td>";
+	    echo "\t";
             echo "<td>".$res['datee']."</td>";
-            echo "<td><a href=\"edit.php?id = $res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
-        }
+            echo "<td><a href=\"edit.php?name=$res[name]\"> Edit </a>
+| <a href=\"delete.php?name=$res[name]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+
+    }
+}
         ?>
-    </table>
 </body>
-</html> 
+</html>
